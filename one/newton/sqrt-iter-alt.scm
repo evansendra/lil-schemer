102eddf651a907721290205904882788edba291c
Evan <evan@evansendra.com>
2021-03-27 21:42:15 +0100

    add ex1.7

--- one/newton/sqrt-iter-alt.scm
+++ one/newton/sqrt-iter-alt.scm
@@ -1,12 +1,11 @@
 (load "new-if.scm")
 
-; compares one guess and the next until the two guesses are within 0.001 of each other meaning we've reached sufficient approximation of sqrt(x)
+; compares one guess and the next without adding variable until the two guesses are within 0.001 of each other meaning we've reached sufficient approximation of sqrt(x)
 
-(define (sqrt-iter prev-guess guess x)
-  (if (good-enough-relative? prev-guess guess x)
+(define (sqrt-iter guess x)
+  (if (good-enough-relative? guess x)
       guess
-      (sqrt-iter guess
-                (improve guess x)
+      (sqrt-iter (improve guess x)
                  x)))
 
 (define (improve guess x)
@@ -19,8 +18,8 @@
   (< (abs (- (square guess) x)) 0.001))
 
 ; An alternative strategy for implementing good-enough? is to watch how guess changes from one iteration to the next and to stop when the change is a very small fraction of the guess. Design a square-root procedure that uses this kind of end test. Does this work better for small and large numbers?
-(define (good-enough-relative? prev-guess guess x)
-  (< (abs (- guess prev-guess)) 0.001))
+(define (good-enough-relative? guess x)
+  (< (abs (- guess (improve guess x))) 0.001))
 
 (define (sqrt x)
-  (sqrt-iter 1.0 2.0 x))
+  (sqrt-iter 1.0 x))
