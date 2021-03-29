406f29e9b01858c283fe3252d8fd57c2df98e68d
Evan <evan@evansendra.com>
2021-03-29 09:48:06 +0200

    cleanup code and edge cases for Newton's method of X^(1/2) and X^(1/3)

--- one/newton/cbrt.scm
+++ one/newton/cbrt.scm
@@ -4,30 +4,30 @@
 ; ----------
 ;      3
 
+(define (square guess)
+  (* guess guess))
+
 (define (cbrt-iter-init y x)
-  (cbrt-iter y (+ y 1) x))
+  (cbrt-iter y (+ y 1.1) x))
 
 (define (cbrt-iter prev-guess guess x)
   (if (good-enough? prev-guess guess x)
       guess
-      (cbrt-iter guess
-                (improve guess x)
-                 x)))
+      (cbrt-iter guess (improve guess x) x)))
 
-(define (approx x guess)
+(define (improve guess x)
   (/
     (+ (/ x (square guess)) (* 2 guess))
     3))
 
-(define (improve guess x)
-  (average guess (approx x guess)))
-
-(define (average x y)
-  (/ (+ x y) 2))
+(define (print n)
+  (display n) (newline))
 
 ; An alternative strategy for implementing good-enough? is to watch how guess changes from one iteration to the next and to stop when the change is a very small fraction of the guess. Design a square-root procedure that uses this kind of end test. Does this work better for small and large numbers?
 (define (good-enough? prev-guess guess x)
-  (< (abs (- guess prev-guess)) 0.001))
+  ; below line would give answer to best of system's precision - iterating until guess and prev-guess are "equal"
+  ; (= guess prev-guess))
+  (< (abs (- guess prev-guess)) (abs (* guess 0.001))))
 
 (define (cbrt x)
-  (cbrt-iter-init 1.0 x))
+  (cbrt-iter-init 0 x))
--- one/newton/sqrt-iter.scm
+++ one/newton/sqrt-iter.scm
@@ -2,6 +2,9 @@
 
 ; compares one guess and the next until the two guesses are within 0.001 of each other meaning we've reached sufficient approximation of sqrt(x)
 
+(define (square guess)
+  (* guess guess))
+
 (define (sqrt-iter prev-guess guess x)
   (if (good-enough-relative? prev-guess guess x)
       guess
@@ -19,8 +22,9 @@
   (< (abs (- (square guess) x)) 0.001))
 
 ; An alternative strategy for implementing good-enough? is to watch how guess changes from one iteration to the next and to stop when the change is a very small fraction of the guess. Design a square-root procedure that uses this kind of end test. Does this work better for small and large numbers?
+; TODO add (* guess 0.001) as test
 (define (good-enough-relative? prev-guess guess x)
-  (< (abs (- guess prev-guess)) 0.001))
+  (< (abs (- guess prev-guess)) (abs (* guess 0.001))))
 
 (define (sqrt x)
   (sqrt-iter 1.0 2.0 x))
