16a957d9c5a4d76750078d873052509f6798f1ca
Evan <evan@evansendra.com>
2021-04-10 00:54:17 +0200

    add recursive fibonacci and explanation of time space complexity

---
+++ one/two/two/fib.scm
@@ -0,0 +1,7 @@
+; @param n number the number for which to compute the fibonacci sequence
+; n >= 0
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
