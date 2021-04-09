e2d6efa0450f50fae175c2569be7dffeb6b804e6
Evan <evan@evansendra.com>
2021-04-10 01:09:07 +0200

    iterative fib

--- one/two/two/fib.scm
+++ one/two/two/fib.scm
@@ -5,3 +5,13 @@
         ((= n 1) 1)
         (else (+ (fib (- n 1))
                  (fib (- n 2))))))
+
+(define (fib-iter n)
+  (define (fib-iterator a b i)
+    (if (= i 0)
+      a
+      (fib-iterator (+ a b) a (- i 1))))
+
+  (if (< n 2)
+    n
+    (fib-iterator 0 1 n)))
