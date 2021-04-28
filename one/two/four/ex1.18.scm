062452ba5e95dec7c1591cdf62bfe0c0117ec979
Evan <evan@evansendra.com>
2021-04-28 20:59:17 +0200

    fix odd calculation break even...

--- one/two/four/ex1.18.scm
+++ one/two/four/ex1.18.scm
@@ -3,27 +3,23 @@
   (define (*i a b c)
     ; store in c the resulting product so far which shouldn't change
     (cond ((= b 0) c)
-        ((even? b) (double (*i a (halve b) c)))
+        ((even? b) (*i a (halve b) (double c)))
         (else (*i a (- b 1) (+ a c))))
   )
   (*i a b 0)
 )
 
 ; (* 2 2)
-; (*i 2 2 0)
-; (double (*i 2 (halve 2) 0))
-; (double (*i 2 1 0))
-; (double (*i 2 0 2))
-; (double 2)
-; 4
+; broken
+
 
 ; (* 2 3)
 ; (*i 2 3 0)
 ; (*i 2 2 2)
-; (double (*i 2 (halve 2) 2))
-; (double (*i 2 1 2))
-; (double (*i 2 0 4))
-; 8
+; (*i 2 (halve 2) (double 2))
+; (*i 2 1 4)
+; (*i 2 0 6)
+; 6
 
 (define (halve a)
   (/ a 2))
