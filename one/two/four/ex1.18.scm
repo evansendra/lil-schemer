9c13ed3b5ac1be343823f1b9e9513e87e0f53ad0
Evan <evan@evansendra.com>
2021-04-29 14:44:50 +0200

    solve 1.18

--- one/two/four/ex1.18.scm
+++ one/two/four/ex1.18.scm
@@ -1,28 +1,29 @@
 ; Using the results of exercises 1.16 and 1.17, devise a procedure that generates an iterative process for multiplying two integers in terms of adding, doubling, and halving and uses a logarithmic number of steps
 (define (* a b)
+
+  ; store in c the resulting product so far
   (define (*i a b c)
-    ; store in c the resulting product so far which shouldn't change
     (cond ((= b 0) c)
-        ((even? b) (*i a (halve b) (double c)))
-        (else (*i a (- b 1) (+ a c))))
-  )
-  (*i a b 0)
-)
+        ((even? b) (*i (double a) (halve b) c))
+        (else (*i a (- b 1) (+ a c)))))
 
-; (* 2 2)
-; broken
-
-
-; (* 2 3)
-; (*i 2 3 0)
-; (*i 2 2 2)
-; (*i 2 (halve 2) (double 2))
-; (*i 2 1 4)
-; (*i 2 0 6)
-; 6
+  (*i a b 0))
 
 (define (halve a)
   (/ a 2))
 
 (define (double a)
   (+ a a))
+
+; (* 2 2)
+; (* 2 2 0)
+; (*i 4 1 0)
+; (*i 4 0 4)
+; 4
+
+; (* 2 3)
+; (*i 2 3 0)
+; (*i 2 2 2)
+; (*i 4 1 2)
+; (*i 4 0 6)
+; 6
