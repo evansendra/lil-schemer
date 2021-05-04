bfd67d26506a04ff945d737dffdddf6fd9b2cfdc
Evan <evan@evansendra.com>
2021-05-04 20:10:58 +0200

    solve 1.19

--- one/two/four/ex1.19.scm
+++ one/two/four/ex1.19.scm
@@ -1,19 +1,9 @@
 ; Now consider T to be the special case of p = 0 and q = 1 in a family of transformations Tpq, where Tpq transforms the pair (a,b) according to a  bq + aq + ap and b  bp + aq. Show that if we apply such a transformation Tpq twice, the effect is the same as using a single transformation Tp'q' of the same form, and compute p' and q' in terms of p and q.
 
 ; Show that if we apply such a transformation Tpq twice, the effect is the same as using a single transformation Tp'q' of the same form
-
-; Tpq(0,1) a=1,b=0 => a <- a+b; b<- a => a <- 1; b<- 1
-; Tpq(0,1)(Tpq(0,1)) a=1,b=1 => a <- 2; b <- 1
-
-; Tp'q'(0,1) = Tpq(0,1)(Tpq(0,1)) => a = (a+b) ; b = (a) => a <- (a)q + (a+b)q + (a+b)p; b <- (a)p + (a+b)q
-; Tp'q'(0,1) a=1, b=0 => a <- 2a+b; b <- a+b => a <- 2; b <- 1
-
 ; compute p' and q' in terms of p and q.
 
-; Tpq(a,b) => a <- bq + aq + ap; b <- bp + aq
-
-; Tp'q'(a,b) = Tpq(0,1)(Tpq(0,1)) => a = bq + aq + ap ; b = bp + aq => ...
-; ...
+; both of the above done on paper here - https://photos.app.goo.gl/AqKaR84np8pNDSheA
 
 ; Put this all together to complete the following procedure, which runs in a logarithmic number of steps:
 
@@ -25,8 +15,8 @@
         ((even? count) 
         (fib-iter a
                   b
-                  ; TODO p'
-                  ; TODO q'
+                  (+ (* p p) (* q q)); p'
+                  (+ (* q q) (* 2 p q)); q'
                   (/ count 2)))
         (else (fib-iter (+ (* b q) (* a q) (* a p))
                         (+ (* b p) (* a q))
