65368c36661728c2215d28dcd0695344ca2afe0f
Evan <evan@evansendra.com>
2021-05-10 19:22:18 +0200

    add prime brute force program

---
+++ one/two/six/div-search.scm
@@ -0,0 +1,14 @@
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+; If d is a divisor of n, then so is n/d. But d and n/d cannot both be greater than n.
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
