5927c4c202159901ecbf2737084f066d1c2b5a26
Evan <evan@evansendra.com>
2021-05-13 19:26:57 +0200

    add fermat test and prime test code

--- one/two/six/fast-prime.scm
+++ one/two/six/fast-prime.scm
@@ -1,3 +1,15 @@
+; run fermat-test times number of times return true if it is true every time else false
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+; run the fermat-test on a given n with a random a, testing if a^n mod n congruent a mod n
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
 ; calculate the exponential of a number mod another number
 (define (expmod base exp m)
   (cond ((= exp 0) 1)
