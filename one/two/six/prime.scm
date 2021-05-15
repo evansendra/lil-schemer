680947dac6ebe3141de8cc9cb0a8a38bce2c0837
Evan <evan@evansendra.com>
2021-05-15 12:33:22 +0200

    add ex 1.21

--- one/two/six/prime.scm
+++ one/two/six/prime.scm
@@ -2,6 +2,7 @@
   (find-divisor n 2))
 
 ; If d is a divisor of n, then so is n/d. But d and n/d cannot both be greater than n.
+; theta(sqrt(n)) operations
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
