94edf543d744fe05d874af0056764c8f01d96695
Evan <evan@evansendra.com>
2021-05-26 10:37:53 +0200

    start 1.3

---
+++ one/three/one/higher-order-procedures.scm
@@ -0,0 +1,48 @@
+(define (sum-integers a b)
+  (if (> a b)
+      0
+      (+ a (sum-integers (+ a 1) b))))
+
+(define (sum-cubes a b)
+  (if (> a b)
+      0
+      (+ (cube a) (sum-cubes (+ a 1) b))))
+
+(define (pi-sum a b)
+  (if (> a b)
+      0
+      (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+
+; general procedure to abstract all of these
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(define (identity x) x)
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+;    1         1         1              π
+;  -----  +  -----  +  -----  +  ... ≈ ---
+;  1 * 3     5 * 7     9 * 11           8 
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+; approximate π
+; (* 8 (pi-sum 1 1000))
+
+; some magic here, going to have to think through again
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b) 
+  dx))
