fcd44149471fc00cf8248fdecb395db3113cd087
Evan <evan@evansendra.com>
2021-06-11 16:51:27 +0200

    finish 1.39

--- one/three/three/1.39.scm
+++ one/three/three/1.39.scm
@@ -7,17 +7,27 @@
 ; 							 -------
 ; 								5 - ..
 ;  where x is in radians. Define a procedure (tan-cf x k) that computes an approximation to the tangent function based on Lambert’s formula. k specifies the number of terms to compute, as in Exercise 1.37.
-(define (pow a b)
-	(if (= b 0)
-			1
-			(* a (pow a (- b 1)))))
-
 (define (tan-cf x k)
+	; intstead of re-writing cont-frac-iter, we could repurpose a more abstract version of cont-frac-iter where the operation performed in the denominator is abstracted so we could use "/" instead of "*" as in 1.37
 	(define (cont-frac-iter count n d res)
 		(if (= count 0)
 				res
 				(cont-frac-iter (- count 1) n d (/ (n count) (- (d count) res)))))
 	(cont-frac-iter k
-									(lambda (i) (pow x i))
+									(lambda (i) (if (< i 2) x (square x))) ; raise to the 2nd power if k>2
 									(lambda (i) (- (* 2 i) 1))
 									0))
+
+; usage
+
+; 1 ]=> (tan-cf 2.0 3)
+
+; Value: -2.1850398632626273
+
+; 1 ]=> (tan-cf 3.14159 10)
+
+;Value: -2.6554829755074223e-6
+
+; 1 ]=> (tan-cf 555.0 1000)
+
+;Value: -1.7924004878260433
