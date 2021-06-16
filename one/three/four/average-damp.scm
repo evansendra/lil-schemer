c2b78b072a880a373acde7329a3365bb6cbe9d57
Evan <evan@evansendra.com>
2021-06-16 23:02:34 +0200

    add better comment to average-damp

--- one/three/four/average-damp.scm
+++ one/three/four/average-damp.scm
@@ -1,6 +1,7 @@
 (load "../average.scm")
 
 ; returns a procedure whose value is the average of x and f(x)
+; in other words, a function which averages an input with the value of a function f at the input
 ; example usage: ((average-damp square) 10) -> 55
 (define (average-damp f)
 	(lambda (x) (average x (f x))))
