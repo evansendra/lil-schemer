7425ed5f5ca6bf07d140cf82fff2c27c23a672b3
Evan <evan@evansendra.com>
2021-06-22 20:05:09 +0200

    add some tests of 1.40 with wolfram alpha

--- one/three/four/cubic.scm
+++ one/three/four/cubic.scm
@@ -3,3 +3,10 @@
 ; to approximate zeros of the cubic x3 + ax2 + bx + c.
 (define (cubic a b c)
 	(lambda (x) (+ (cube x) (* a (square x)) (* b x) c)))
+
+; example:
+; 1 ]=> (newtons-method (cubic 3 2 1) 1)
+;Value: -2.3247179572447267
+
+; 1 ]=> (newtons-method (cubic 16 4 326) 1)
+;Value: -16.90421954849738
