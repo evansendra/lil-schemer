43a33128b004692ce2487ae9efeca888f7af6a61
Evan <evan@evansendra.com>
2021-06-29 11:42:56 +0200

    begin ch2

---
+++ two/one/one/rational.scm
@@ -0,0 +1,31 @@
+; assumptions
+; (make-rat <n> <d>) returns the rational number whose numerator is the integer <n> and whose denominator is the integer <d>
+; (numer <x>) returns the numerator of the rational number <x>
+; (denom <x>) returns the denominator of the rational number <x>
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+; data abstraction
+(define (make-rat n d) (cons n d))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(define (print-rat x)
+	(newline)
+	(display (numer x))
+	(display "/")
+	(display (denom x)))
