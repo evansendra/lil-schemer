11a7dd536c6ef6afa3b3f11d39907c06d7585c4c
Evan <evan@evansendra.com>
2021-07-01 13:07:50 +0200

    breakout testing code into util

---
+++ utils/testing.scm
@@ -0,0 +1,16 @@
+; allows to compare an actual result to an expected result of a computation optionally passing
+; in a custom comparator (= is default) 
+(define (testc actual expected comparator)
+	(let ((res (comparator expected actual)))
+	(display (if res "✅" "🚫"))
+	(newline)
+	(display "expected: ")
+	(display expected)
+	(newline)
+	(display "actual: ")
+	(display actual)
+	(newline)
+	res))
+
+(define (test actual expected)
+	(testc actual expected =))
