71338a02ecbe67130f5e8090e16e40af3e768499
Evan <evan@evansendra.com>
2021-07-07 10:19:15 +0200

    add ex1.10

--- two/one/four/interval-arithmetic.scm
+++ two/one/four/interval-arithmetic.scm
@@ -10,7 +10,7 @@
     (make-interval (min p1 p2 p3 p4)
                    (max p1 p2 p3 p4))))
 
-(define (div-interval x y)
+(define (div-interval-orig x y)
   (mul-interval x 
                 (make-interval (/ 1.0 (upper-bound y))
                                (/ 1.0 (lower-bound y)))))
@@ -33,3 +33,12 @@
 (define (sub-interval-alt x y)
 	(add-interval x
 								(make-interval (- (lower-bound y)) (- (upper-bound y)))))
+
+; Exercise 2.10.  Ben Bitdiddle, an expert systems programmer, looks over Alyssa's shoulder and comments that it is not clear what it means to divide by an interval that spans zero. Modify Alyssa's code to check for this condition and to signal an error if it occurs.
+(define (div-interval x y)
+  (if (= (upper-bound y) (lower-bound y))
+    (error "division by zero interval")
+    (mul-interval x 
+      (make-interval (/ 1.0 (upper-bound y))
+                      (/ 1.0 (lower-bound y))))))
+  
