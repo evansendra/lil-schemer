16e6b693b1d816f3868e1b039add36bcbf499c44
Evan <evan@evansendra.com>
2021-08-06 15:24:43 +0200

    write working 'fringe' 2.28

---
+++ two/two/two/ex2.28.scm
@@ -0,0 +1,24 @@
+; Exercise 2.28.  Write a procedure fringe that takes as argument a tree (represented as a list) and returns a list whose elements are all the leaves of the tree arranged in left-to-right order. For example,
+
+(define x (list (list 1 2) (list 3 4)))
+
+(fringe x)
+; (1 2 3 4)
+
+(fringe (list x x))
+; (1 2 3 4 1 2 3 4)
+
+(define (fringe x)
+  (cond ((null? x) ())
+        ((pair? (car x)) (append (fringe (car x)) (fringe (cdr x))))
+        (else (cons (car x) (fringe (cdr x))))))
+
+(load "../../../utils/testing.scm")
+(define (list= l1 l2)
+  (cond ((null? l1) true)
+        ((= (car l1) (car l2)) (list= (cdr l1) (cdr l2)))
+        (else false)))
+
+(testc (fringe x) (list 1 2 3 4) list=)
+(testc (fringe (list x x)) (list 1 2 3 4 1 2 3 4) list=)
+(testc (fringe (list x (list (list (list x))))) (list 1 2 3 4 1 2 3 4) list=)
