f7ce30444b45b0c73bb48a74bb244d62a566bd01
Evan <evan@evansendra.com>
2021-09-05 22:01:03 +0200

    ex 2.30 square-tree with and w/o map

---
+++ two/two/two/ex2.30.scm
@@ -0,0 +1,33 @@
+; Exercise 2.30.  Define a procedure square-tree analogous to the square-list procedure of exercise 2.21. That is, square-list should behave as follows:
+
+; (square-tree
+;  (list 1
+;        (list 2 (list 3 4) 5)
+;        (list 6 7)))
+; (1 (4 (9 16) 25) (36 49))
+
+; Define square-tree both directly (i.e., without using any higher-order procedures) and also by using map and recursion.
+
+(define l (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+(define nil '())
+(define (square x) (* x x))
+
+; without higher order procedures
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+; with recursion and map
+(define (square-tree-map tree)
+  (map (lambda (subtree)
+        (if (pair? subtree) 
+          (square-tree-map subtree)
+          (square subtree)))
+    tree))
+
+(load "../../../utils/testing.scm")
+(define (list-deep= l1 l2))
+(testc (square-tree l) (list 1 (list 4 (list 9 16) 25) (list 36 49)) list=)
+(testc (square-tree-map l) (list 1 (list 4 (list 9 16) 25) (list 36 49)) list=)
