fc20e8c643089397abd0140a194f8b684a7e2f35
Evan <evan@evansendra.com>
2021-09-21 23:07:11 +0200

    add nil definition and normal fibs in addition to even fibs

--- two/two/three/even-fibs.scm
+++ two/two/three/even-fibs.scm
@@ -1,5 +1,15 @@
 (load "../../../one/two/two/fib.scm")
 
+(define nil '())
+
+(define (fibs n)
+  (define (next k)
+    (if (> k n)
+      nil
+      (let ((f (fib-iter k)))
+        (cons f (next (+ k 1))))))
+  (next 0))
+
 (define (even-fibs n)
   (define (next k)
     (if (> k n)
