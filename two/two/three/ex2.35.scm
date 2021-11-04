6173cd058e133f2b088984126e7c408d507c4c23
Evan <evan@evansendra.com>
2021-11-04 17:50:17 -0500

    complete exercise 2.35.scm

--- two/two/three/ex2.35.scm
+++ two/two/three/ex2.35.scm
@@ -12,29 +12,6 @@
         (else (+ (count-leaves (car x))
                  (count-leaves (cdr x))))))
 
-(define (count-leaves t)
-  (accumulate (lambda (first rest) (+ 1 rest)) 0 
-    (map (lambda (node) (if (pair? node) (count-leaves node) node)) t))) 
-
-; problem to be solved:
-
-; 5 error> (define l (list 1 2 3 (list 4 5) (list 6 7) (list 8 (list 9))))
-
-; ;Value: l
-
-; 5 error> l
-
-; ;Value: (1 2 3 (4 5) (6 7) (8 (9)))
-
-; 5 error> (count-leaves l)
-
-; ;Value: 6
-
-; 5 error> (load "../two/count-leaves.scm")
-
-; ;Loading "../two/count-leaves.scm"... done
-; ;Value: count-leaves
-
-; 5 error> (count-leaves l)
-
-; ;Value: 9
+(define (count-leaves-acc t)
+  (accumulate + 0 
+    (map (lambda (node) (if (pair? node) (count-leaves-acc node) 1)) t))) 
