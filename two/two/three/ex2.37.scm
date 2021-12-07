53bdf0fd511b9064ecdd7160e283187bf195f256
Evan <evan@evansendra.com>
2021-12-06 22:03:31 -0600

    get matrix-*-vector working

---
+++ two/two/three/ex2.37.scm
@@ -0,0 +1,21 @@
+(load "accumulate.scm")
+(define nil '())
+
+(define v1 (list 1 2))
+(define v2 (list 3 4))
+(define m1 (list v1 v2))
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v) 
+  (map (lambda (i) (accumulate + 0 (accumulate-n * 1 (list i v)))) m))
+  ; for each row i in m
+    ; create a sum of 
+      ; multiplication mij * vj
+
+; testing...
+; m1 * v1 = 1*1 + 2*2 = 5
+;           3*1 + 4*2   11
+; aka
+; (matrix-*-vector m1 v1) -> (5 11)
