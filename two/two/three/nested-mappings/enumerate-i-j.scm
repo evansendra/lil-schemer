eab626ae656ade6d64b8e6dad21fe4039d6d2431
Evan <evan@evansendra.com>
2022-01-04 19:53:48 +0100

    add enumeration of ordered pairs such that 1 < j < i <= n using accumulate and map from text

---
+++ two/two/three/nested-mappings/enumerate-i-j.scm
@@ -0,0 +1,17 @@
+(load "../accumulate.scm")
+
+(define nil '())
+(define n 6) ; could be any integer >= 1
+
+; alternative implementation: two/two/three/signal-flow-even-fibs.scm
+(define (enumerate-interval low high)
+  (define (iter low high seq)
+    (if (> low high)
+        seq
+        (iter (+ low 1) high (append seq (list low)))))
+  (iter low high nil))
+
+(accumulate
+  append nil (map (lambda (i)
+               (map (lambda (j) (list i j)) (enumerate-interval 1 (- i 1))))
+                  (enumerate-interval 1 n)))
