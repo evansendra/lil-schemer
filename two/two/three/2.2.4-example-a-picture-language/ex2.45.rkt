dc2c0e9cb81dad24e1d4e40246f9ed2596e14d7c
Evan <evan@evansendra.com>
2022-01-17 20:12:39 +0100

    add split a la exercise 2.45

--- two/two/three/2.2.4-example-a-picture-language/ex2.45.rkt
+++ two/two/three/2.2.4-example-a-picture-language/ex2.45.rkt
@@ -2,11 +2,17 @@
 (require sicp-pict)
 (include "example-code.rkt")
 
-;(define (split op1 op2) ...)
+(define (split op1 op2)
+  (define (split-n painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (split-n painter (- n 1))))
+          (op1 painter (op2 smaller smaller)))))
+  split-n)
 
 
 ;Exercise 2.45: right-split and up-split can be expressed as instances of a general splitting operation.
 ;Define a pro- cedure split with the property that evaluating
-(define right-split (split beside below))
-(define up-split (split below beside))
+(define right-split-2 (split beside below))
+(define up-split-2 (split below beside))
 ;produces procedures right-split and up-split with the same behaviors as the ones already defined.
