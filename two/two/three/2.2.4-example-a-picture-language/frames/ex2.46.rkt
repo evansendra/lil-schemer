f050c9c4924d3c0318ade8c2d3225fc57712e5f5
Evan <evan@evansendra.com>
2022-01-24 10:39:18 +0100

    implement add-vect

--- two/two/three/2.2.4-example-a-picture-language/frames/ex2.46.rkt
+++ two/two/three/2.2.4-example-a-picture-language/frames/ex2.46.rkt
@@ -17,6 +17,9 @@
 (define (ycor-vect v)
   (cdr v))
 
-;(define (add-vect v1 v2))
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+             (+ (ycor-vect v1) (ycor-vect v2))))
+
 ;(define (sub-vect v1 v2))
 ;(define (scale-vect v s))
