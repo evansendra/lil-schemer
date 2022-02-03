5a8a1bd8ad2ed8e6b834d8726addb36e279c690f
Evan <evan@evansendra.com>
2022-02-03 22:45:24 +0100

    "add ex2.48 boilerplate"

---
+++ two/two/three/2.2.4-example-a-picture-language/painters/ex2.48.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+;Exercise 2.48: A directed line segment in the plane can be represented as a pair of vectors—the vector running from the origin to the start-point of the segment, and the vector running from the origin to the end-point of the segment. Use your vector representation from Exercise 2.46 to de- fine a representation for segments with a constructor make- segment and selectors start-segment and end-segment.
+
+(define (make-segment p1 p2))
+(define (start-segment s))
+(define (end-segment s))
