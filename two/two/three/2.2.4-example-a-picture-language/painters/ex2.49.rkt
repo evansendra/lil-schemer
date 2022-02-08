d9c548bff5b1a6e933751028296aafaa31074f85
Evan <evan@evansendra.com>
2022-02-08 18:38:56 +0100

    "add ex2.49 prompt"

---
+++ two/two/three/2.2.4-example-a-picture-language/painters/ex2.49.rkt
@@ -0,0 +1,6 @@
+#lang racket
+;Exercise 2.49: Use segments->painter to define the fol- lowing primitive painters:
+;a. The painter that draws the outline of the designated frame.
+;b. the painterthatdrawsan“X”byconnectingopposite corners of the frame.
+;c. The painter that draws a diamond shape by connect- ing the midpoints of the sides of the frame.
+;d. The wave painter.
