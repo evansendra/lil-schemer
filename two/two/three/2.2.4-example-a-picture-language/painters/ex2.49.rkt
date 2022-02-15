cbb09f86b4b02fbd4ccd2ca6fe0d601a82b712af
Evan <evan@evansendra.com>
2022-02-15 20:23:08 +0100

    add dimaond painter

--- two/two/three/2.2.4-example-a-picture-language/painters/ex2.49.rkt
+++ two/two/three/2.2.4-example-a-picture-language/painters/ex2.49.rkt
@@ -23,5 +23,13 @@
      (segment (vect 0.0 1.0) (vect 1.0 0.0)))))
 ; (paint x-painter)
 
-;(define (diamond))
+(define diamond 
+  (segments->painter 
+   (list 
+    (segment (vect 0.0 0.5) (vect 0.5 0.0)) 
+    (segment (vect 0.5 0.0) (vect 1.0 0.5)) 
+    (segment (vect 1.0 0.5) (vect 0.5 1.0)) 
+    (segment (vect 0.5 1.0) (vect 0.0 0.5)))))
+; (paint diamond)
+
 ;(define (wave))
