11deb644e9cdf632fd3e619d1ce349c667aeb4a9
Evan <evan@evansendra.com>
2022-02-21 19:29:55 +0100

    use sicp-pict for example code and add more from transforming painters

--- two/two/three/2.2.4-example-a-picture-language/painters/transform-painter.rkt
+++ two/two/three/2.2.4-example-a-picture-language/painters/transform-painter.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (require sicp-pict)
-(require "../frames/ex2.46.rkt")
+;(require "../frames/ex2.46.rkt")
 
 (define (transform-painter painter origin corner1 corner2)
   (lambda (frame)
@@ -8,5 +8,12 @@
       (let ((new-origin (m origin)))
         (painter (make-frame
                   new-origin
-                  (sub-vect (m corner1) new-origin)
-                  (sub-vect (m corner2) new-origin)))))))
+                  (vector-sub (m corner1) new-origin)
+                  (vector-sub (m corner2) new-origin)))))))
+
+; flipping an image vertically using ``transform-painter``
+(define (flip-vert painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0) ; new origin
+                     (make-vect 1.0 1.0) ; new end of edge1
+                     (make-vect 0.0 0.0))); new end of edge2
