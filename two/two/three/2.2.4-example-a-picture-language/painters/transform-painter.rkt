7e2f86853c1ec8b8750df3fb9fa47abe5250ddc5
Evan <evan@evansendra.com>
2022-02-22 18:24:53 +0100

    add more examples of transformations

--- two/two/three/2.2.4-example-a-picture-language/painters/transform-painter.rkt
+++ two/two/three/2.2.4-example-a-picture-language/painters/transform-painter.rkt
@@ -17,3 +17,11 @@
                      (make-vect 0.0 1.0) ; new origin
                      (make-vect 1.0 1.0) ; new end of edge1
                      (make-vect 0.0 0.0))); new end of edge2
+
+; shrink image to upper-right quarter of frame
+(define (shrink-to-upper-right painter)
+  (transform-painter
+   painter
+   (make-vect 0.5 0.5)
+   (make-vect 1.0 0.5)
+   (make-vect 0.5 1.0)))
