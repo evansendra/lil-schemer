f5c8f23cf573d56c276b788004b9a50d3e4944de
Evan <evan@evansendra.com>
2022-03-10 20:31:10 +0100

    add next exercise

---
+++ two/two/three/2.2.4-example-a-picture-language/painters/ex2.50.rkt
@@ -0,0 +1,2 @@
+#lang racket
+; Exercise 2.50: Define the transformation flip-horiz, which flips painters horizontally, and transformations that rotate painters counterclockwise by 180 degrees and 270 degrees.
