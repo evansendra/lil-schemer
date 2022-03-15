1f506a424c03b4cc8b45dfc6393a71430c783ca4
Evan <evan@evansendra.com>
2022-03-15 23:51:56 +0100

    add flip-horiz

--- two/two/three/2.2.4-example-a-picture-language/painters/ex2.50.rkt
+++ two/two/three/2.2.4-example-a-picture-language/painters/ex2.50.rkt
@@ -1,2 +1,10 @@
 #lang racket
+(require sicp-pict)
+(require "./transform-painter.rkt")
+
 ; Exercise 2.50: Define the transformation flip-horiz, which flips painters horizontally, and transformations that rotate painters counterclockwise by 180 degrees and 270 degrees.
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
