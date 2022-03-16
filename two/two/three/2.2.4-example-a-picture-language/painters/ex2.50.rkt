9b1221403812da9e7ca40d386ff2d3c8418d1eb1
Evan <evan@evansendra.com>
2022-03-16 23:29:19 +0100

    add rotate180

--- two/two/three/2.2.4-example-a-picture-language/painters/ex2.50.rkt
+++ two/two/three/2.2.4-example-a-picture-language/painters/ex2.50.rkt
@@ -8,3 +8,9 @@
                      (make-vect 1.0 0.0)
                      (make-vect 0.0 0.0)
                      (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
