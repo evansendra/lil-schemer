976384f43c62e0a26a996ca7de19191f25adb486
Evan <evan@evansendra.com>
2022-03-22 23:57:57 +0100

    add ex2.52 prompt

---
+++ two/two/three/2.2.4-example-a-picture-language/painters/ex2.52.rkt
@@ -0,0 +1,7 @@
+#lang racket
+(include sicp-pict)
+;
+;Exercise 2.52: Make changes to the square limit of wave shown in Figure 2.9 by working at each of the levels de- scribed above. In particular:
+;a. Add some segments to the primitive wave painter of Exercise 2.49 (to add a smile, for example).
+;b. Changethepaernconstructedbycorner-split(for example, by using only one copy of the up-split and right-split images instead of two).
+;c. Modifytheversionofsquare-limitthatusessquare- of-four so as to assemble the corners in a different paern. (For example, you might make the big Mr. Rogers look outward from each corner of the square.)
