522a29acb55d35f1c94318a562c00f189c1065de
Evan <evan@evansendra.com>
2022-04-09 00:23:00 +0200

    explain 2.55

--- two/three/ex2.55.scm
+++ two/three/ex2.55.scm
@@ -3,3 +3,5 @@
 (car ''abracadabra)
 
 ; To her surprise, the interpreter prints back quote. Explain.
+
+; we know '... yields (quote ...) but under the hood quote must behave differently than a regular procedure.  This behaves like a list when exposed directly to the interpreter, which is a bit odd (given what we'be read so far).  So instead of showing an error that car can't be used because an incorrect type is passed in to car, we get simply "quote" returned back, showing that 'abracadabra exposed to `car` yields simply quote as cadr would yield abracadabra.
